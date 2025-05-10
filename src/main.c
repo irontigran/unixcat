64f7e0701581e8dd87352edff7fa9a02a1471022
irontigran <22407135+irontigran@users.noreply.github.com>
2025-05-10 16:23:09 -0400

    Add mini getopt parsing for listening or not

--- src/main.c
+++ src/main.c
@@ -1,5 +1,31 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main (int argc, char **argv) {
-    printf("Hello world!\n");
+int main(int argc, char **argv) {
+    int opt;
+    bool listen = false;
+    while ((opt = getopt(argc, argv, "+l")) != -1) {
+        switch (opt) {
+            case 'l':
+                listen = true;
+                break;
+            default:
+                goto usage_exit;
+        }
+    }
+    if (optind >= argc) {
+        goto usage_exit;
+    }
+    char *path = argv[optind];
+    if (listen) {
+        printf("Will listen on %s\n", path);
+    } else {
+        printf("Will connect to %s\n", path);
+    }
+    exit(EXIT_SUCCESS);
+usage_exit:
+    fprintf(stderr, "Usage: %s [-l] path\n", argv[0]);
+    exit(EXIT_FAILURE);
 }

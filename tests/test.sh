ab05878e52aa27c88a9e42205e3a2233734b1e0c
irontigran <22407135+irontigran@users.noreply.github.com>
2025-05-13 14:50:13 -0400

    Add simple test script

---
+++ tests/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+socket=$(mktemp -u sock.XXX)
+results=$(mktemp ucat.XXX)
+
+clean_and_exit() {
+    rm -f "$socket" "$results"
+    exit $1
+}
+
+success=0
+fail=1
+hard_fail=99
+
+(./ucat -l "$socket" > "$results" &) || clean_and_exit $hard_fail
+expected="hi"
+echo -n "$expected" | ./ucat "$socket" || clean_and_exit $hard_fail
+
+stat=$success
+if [ "$(< "$results")" != "$expected" ]; then
+    echo "expected $expected, got $results"
+    stat=$fail
+fi
+clean_and_exit $stat

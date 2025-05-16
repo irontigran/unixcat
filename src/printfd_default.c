b2b1e81ff0a547a53e66c0866296a708b240a614
irontigran <22407135+irontigran@users.noreply.github.com>
2025-05-16 11:10:42 -0400

    Add default print for file descriptor

--- src/printfd_default.c
+++ src/printfd_default.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <sys/stat.h>
 
 #include "printfd.h"
 
-void PFD_print_fd(int fd) { printf("@ANC: SCM_RIGHTS %d\n", fd); }
+void PFD_print_fd(int fd) {
+    struct stat sb;
+    if (fstat(fd, &sb) < 0) {
+        perror("stat");
+    }
+    printf("@ANC: SCM_RIGHTS ");
+    switch (st.st_mode & S_IFMT) {
+        case S_IFSOCK:
+            printf("[socket]");
+            break;
+        case S_IFLNK:
+            printf("[link]");
+            break;
+        case S_IFREG:
+            printf("[file]");
+            break;
+        case S_IFBLK:
+            printf("[block dev]");
+            break;
+        case S_IFDIR:
+            printf("[directory]");
+            break;
+        case S_IFCHR:
+            printf("[char dev]");
+            break;
+        case S_IFIFO:
+            printf("[pipe]");
+            break;
+        default:
+            printf("[?]");
+            break;
+    }
+    printf("\n");
+}

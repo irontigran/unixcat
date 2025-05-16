78087a8a2b2aa91b2ca41cc139d6f5c00f98b6ad
irontigran <22407135+irontigran@users.noreply.github.com>
2025-05-16 11:11:49 -0400

    Add kinfo method for printing fds

---
+++ src/printfd_kinfo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/user.h>
+
+#include "printfd.h"
+
+void PFD_print_fd(int fd) {
+    struct kinfo_file kf;
+    memset(&kf, 0, sizeof(kf));
+    kf.kf_structsize = sizeof(kf);
+    if (fcntl(fd, F_KINFO, &kf) < 0) {
+        perror("fcntl");
+    }
+    printf("@ANC: SCM_RIGHTS %s\n", kf.kf_path);
+}

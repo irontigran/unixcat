98e3b81681417909c8e5c63eb5d0a3026898f599
irontigran <22407135+irontigran@users.noreply.github.com>
2025-05-31 21:24:03 -0400

    Add comments to the ancillary config struct

--- include/main.h
+++ include/main.h
@@ -7,11 +7,24 @@
 // Linux-specific limit, but 253 file descriptors should be enough for most.
 #define SCM_MAX_FD 253
 
+/* Controls the behavior around sending and receiving ancillary messages.
+ */
 typedef struct ancillary_cfg {
+    // The list of file descriptors to send. Set numfds to 0 if there aren't
+    // any.
     int numfds;
     int send_fds[SCM_MAX_FD];
-    int send_creds;  // 0 means don't, negative means forever.
+
+    // For both send_ and recv_creds, a negative value means send or receive
+    // creds on every message, 0 means don't send/recv creds, and a positive
+    // number means send/recv creds only on the first message. Possible
+    // extension: positive number means send/recv creds on the first n
+    // messages.
+    int send_creds;
     int recv_creds;
+
+    // Linux only: set the pid/uid/gid of credentials. Ignored by other
+    // systems.
     pid_t pid;
     uid_t uid;
     gid_t gid;

c4d0e302326e6c1fbbd2983af8f4e49c43e3a179
irontigran <22407135+irontigran@users.noreply.github.com>
2025-06-06 17:05:13 -0400

    Add stub functions for disabled creds

--- src/creds/creds_disabled.c
+++ src/creds/creds_disabled.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 
 #include "creds.h"
+#include "main.h"
 #include "options.h"
 
 char *Creds_help_message = "";
@@ -16,3 +17,7 @@ int Creds_confirm_recv_settings(int fd) { return 0; }
 bool Creds_is_credential(int cmsg_type) { return false; }
 
 void Creds_print_credential(struct cmsghdr *cmsg) { return; }
+
+size_t Creds_sizeof_send_struct(void) { return 0; }
+
+void Creds_fill_cmsg(struct cmsghdr *cmsg, AncillaryCfg cfg) { return; }
